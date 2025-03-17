a5c4b040d9d75803479d9b02223fb2d09999398d
dyns <dylan.schumacker@outlook.fr>
2025-03-17 10:40:47 +0100

    ⚙️ init: spring project

---
+++ src/main/java/com/dyns/api_fitness_buddy/ApiFitnessBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.dyns.api_fitness_buddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFitnessBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFitnessBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dyns/api_fitness_buddy/ApiFitnessBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dyns.api_fitness_buddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFitnessBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
